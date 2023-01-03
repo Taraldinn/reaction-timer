31e90ecadc10eebd15a44df27d564e15d91e7d51
Taraldinn <kferdoush617@gmail.com>
2023-01-03 20:51:06 +0600

    done this project

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -1,6 +1,9 @@
 <template>
   <p class="text-center text-2xl font-bold font-body mt-10 ">Reaction Timer  {{ score }} MS</p>
-  <p class="text-center text-2xl font-bold font-body mt-10 ">{{ rank }}</p>
+  <div class="mt-10 flex justify-center ">
+    <p class="rounded  w-24 text-white px-6 py-2 bg-blue-500  text-center text-2xl font-bold font-body  ">{{ rank }}</p>
+
+  </div>
 
 </template>
 
